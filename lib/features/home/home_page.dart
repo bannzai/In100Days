8a923e35f9a7c5120d514dfa21fcef557913823b
bannzai <kingkong999yhirose@gmail.com>
2022-06-05 17:10:22 +0900

    fix twitter api client

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_100_days/provider/twitter_api_client.dart';
+import 'package:in_100_days/secret/secret.dart';
+import 'package:oauth1/oauth1.dart';
 
 class HomePage extends HookConsumerWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final Client twitterAPIClient;
+  const HomePage({Key? key, required this.twitterAPIClient}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final twitterAPIClient = ref.read(twitterAPIClientProvider);
-
     Future.microtask(() async {
       final apiResponse = await twitterAPIClient.get(
         Uri.parse(
