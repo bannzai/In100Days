6398ca0d9179386a4548b003cd4f9800e563e233
bannzai <kingkong999yhirose@gmail.com>
2022-06-05 17:10:29 +0900

    fix provider

--- lib/provider/twitter_api_client.dart
+++ lib/provider/twitter_api_client.dart
@@ -1,19 +1,20 @@
+import 'package:in_100_days/provider/secure_storage.dart';
 import 'package:oauth1/oauth1.dart' as oauth1;
-import 'package:in_100_days/features/login/login.dart';
 import 'package:riverpod/riverpod.dart';
 
 import '../oauth/oatuh1.dart';
 
-final twitterAPIClientProvider = Provider((ref) {
-  final twitterLoginAuthResult = ref.watch(twitterLoginAuthResultProvider)!;
+final twitterAPIClientProvider = Provider((ref) async {
+  final secureStorage = ref.watch(secureStorageProvider);
+  final twitterAuthToken =
+      await secureStorage.read(key: SecuretStorageKeys.twitterAuthToken);
+  final twitterAuthTokenSecret =
+      await secureStorage.read(key: SecuretStorageKeys.twitterAuthTokenSecret);
 
   final client = oauth1.Client(
     oAuth1TwitterPlatform.signatureMethod,
     clientCredentials,
-    oauth1.Credentials(
-      twitterLoginAuthResult.authToken!,
-      twitterLoginAuthResult.authTokenSecret!,
-    ),
+    oauth1.Credentials(twitterAuthToken!, twitterAuthTokenSecret!),
   );
   return client;
 });
