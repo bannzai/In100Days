3b93adbc57da7f5f3192d2e159592c21fd234f00
bannzai <kingkong999yhirose@gmail.com>
2022-06-06 15:01:20 +0900

    Add user delete button

---
+++ lib/features/user_delete/user_delete_button.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:in_100_days/features/error/error_alert.dart';
+import 'package:in_100_days/provider/secure_storage.dart';
+
+class UserDeleteButton extends StatelessWidget {
+  const UserDeleteButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        child: const Text("Delete User"),
+        onPressed: () async {
+          try {
+            const securetStorage = FlutterSecureStorage();
+            await securetStorage.delete(
+                key: SecuretStorageKeys.twitterAuthToken);
+            await securetStorage.delete(
+                key: SecuretStorageKeys.twitterAuthTokenSecret);
+
+            await FirebaseAuth.instance.currentUser?.delete();
+          } catch (error) {
+            showErrorAlert(context, error: error);
+          }
+        });
+  }
+}
