4524faae6847bfc4611bfe2d9b3d23ff0638fd86
bannzai <kingkong999yhirose@gmail.com>
2022-06-07 16:01:28 +0900

    Fix home page

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_100_days/features/error/error_page.dart';
+import 'package:in_100_days/features/goal/goal_input_sheet.dart';
+import 'package:in_100_days/features/home/state_notifier.dart';
 import 'package:in_100_days/provider/auth.dart';
 import 'package:in_100_days/provider/tweets.dart';
 
@@ -10,25 +12,20 @@ class HomePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final tweetsStream = ref.watch(tweetsStreamProvider);
-
-    Future.microtask(() {
-      if (user != null) {
-        showModalBottomSheet(
-          context: context,
-          isScrollControlled: true,
-          builder: (context) {
-            return ObjectiveSheet(twitterIDName: user.screenName);
-          },
-        );
-      }
-    });
-
-    return tweetsStream.when(
-        data: (tweets) {
-          if (tweets.isEmpty) {
-            // TODO:
-            return Container(child: Text("Tweet is Empty"));
+    final state = ref.watch(homeStateNotifierProvider);
+    return state.when(
+        data: (state) {
+          if (state.goals.isEmpty) {
+            Future.microtask(() {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (context) {
+                  return GoalInputSheet(twitterIDName: state.user.screenName);
+                },
+              );
+            });
+            return Container();
           } else {
             // TODO:
             return Container(child: Text("Tweet is not Empty"));
