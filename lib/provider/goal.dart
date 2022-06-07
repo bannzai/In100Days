3f47d9e4c271244bab2c28243809d3ec0f78029e
bannzai <kingkong999yhirose@gmail.com>
2022-06-07 15:54:02 +0900

    add goals stream provider

--- lib/provider/goal.dart
+++ lib/provider/goal.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod/riverpod.dart';
 
 import '../entity/goal.codegen.dart';
 
@@ -21,3 +23,11 @@ CollectionReference<Goal> goalCollectionReference({required String userID}) =>
           fromFirestore: _fromFirestore(),
           toFirestore: _toFirestore(),
         );
+
+final goalsStreamProvider = StreamProvider((ref) {
+  final collectionReference =
+      goalCollectionReference(userID: FirebaseAuth.instance.currentUser!.uid);
+  return collectionReference
+      .snapshots()
+      .map((event) => event.docs.map((e) => e.data()!).toList());
+});
