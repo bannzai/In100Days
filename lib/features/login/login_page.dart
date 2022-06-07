941ac15becb1e323026c78b5c133cedfa4e3ad94
bannzai <kingkong999yhirose@gmail.com>
2022-06-07 14:17:03 +0900

    WIP fix layout

--- lib/features/login/login_page.dart
+++ lib/features/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_100_days/features/error/error_alert.dart';
 import 'package:in_100_days/provider/user.dart';
@@ -57,53 +58,59 @@ class LoginPage extends HookConsumerWidget {
   }
 }
 
-class ObjectiveSheet extends StatelessWidget {
+class ObjectiveSheet extends HookConsumerWidget {
   final String twitterIDName;
 
   const ObjectiveSheet({Key? key, required this.twitterIDName})
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final textFieldController = useTextEditingController(text: "");
+
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              "100日後に",
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 30,
-              ),
-            ),
-            SizedBox(
-              width: 200,
-              height: 200,
-              child: TextField(
-                decoration: const InputDecoration(
-                  filled: true,
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(width: 1),
-                  ),
-                  contentPadding: EdgeInsets.only(bottom: 8),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "#100日後に",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 30,
                 ),
-                onChanged: (text) {
-                  print(text);
-                },
               ),
-            ),
-            Text(
-              twitterIDName,
-              style: const TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 30,
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  controller: textFieldController,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(width: 1),
+                    ),
+                    contentPadding: EdgeInsets.only(bottom: 8),
+                  ),
+                  onChanged: (text) {
+                    print(text);
+                  },
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Text(
+                "@" + twitterIDName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 30,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
