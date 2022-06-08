a9ae41fc480b44753e0cc888a91dab787200d81a
bannzai <kingkong999yhirose@gmail.com>
2022-06-08 21:36:32 +0900

    remove unnecessary imports

--- lib/features/records/record_list.dart
+++ lib/features/records/record_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_100_days/entity/goal.codegen.dart';
 import 'package:in_100_days/features/error/error_page.dart';
--- lib/features/records/record_list_empty.dart
+++ lib/features/records/record_list_empty.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:in_100_days/entity/goal.codegen.dart';
-import 'package:in_100_days/entity/user.codegen.dart';
 import 'package:in_100_days/features/record_add/record_add_sheet.dart';
 import 'package:in_100_days/features/records/state.codegen.dart';
 import 'package:in_100_days/style/color.dart';
