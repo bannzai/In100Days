ce7f726750627c097a6f2e89b38df92a92ad8e0c
bannzai <kingkong999yhirose@gmail.com>
2022-06-16 20:42:30 +0900

    Revert "Use user primary color"
    This reverts commit b85bc1e2bcd482d9bd5b84ffbc6c976c5fe58521.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_100_days/provider/twitter_api_client.dart';
 import 'package:in_100_days/root.dart';
+import 'package:in_100_days/style/color.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,8 +17,22 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ProviderScope(
-      child: Root(),
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: AppColor.primary,
+          inputDecorationTheme: const InputDecorationTheme(
+            border: UnderlineInputBorder(
+              borderSide: BorderSide(width: 1, color: AppColor.primary),
+            ),
+          ),
+          textSelectionTheme: const TextSelectionThemeData(
+            cursorColor: AppColor.primary,
+          ),
+        ),
+        home: const Root(),
+      ),
     );
   }
 }
--- lib/root.dart
+++ lib/root.dart
@@ -4,7 +4,6 @@ import 'package:in_100_days/features/error/error_page.dart';
 import 'package:in_100_days/features/home/home.dart';
 import 'package:in_100_days/features/sign_in/sign_in_page.dart';
 import 'package:in_100_days/provider/user.dart';
-import 'package:in_100_days/style/color.dart';
 
 class Root extends HookConsumerWidget {
   const Root({Key? key}) : super(key: key);
@@ -12,34 +11,18 @@ class Root extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userStreamProvider);
-    final userColor = ref.watch(userColorProvider);
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: userColor ?? AppColor.primary,
-        inputDecorationTheme: const InputDecorationTheme(
-          border: UnderlineInputBorder(
-            borderSide: BorderSide(width: 1, color: Colors.black),
-          ),
-        ),
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: Colors.black,
-          selectionColor: Colors.black,
-        ),
-      ),
-      home: user.when(
-        data: (user) {
-          if (user != null) {
-            return AppHome(user: user);
-          } else {
-            return const SignInPage();
-          }
-        },
-        error: (error, st) => ErrorPage(error: error),
-        loading: () => const Center(
-          child: CircularProgressIndicator(),
-        ),
+    return user.when(
+      data: (user) {
+        if (user != null) {
+          return AppHome(user: user);
+        } else {
+          return const SignInPage();
+        }
+      },
+      error: (error, st) => ErrorPage(error: error),
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
       ),
     );
   }
