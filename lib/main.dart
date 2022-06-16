b85bc1e2bcd482d9bd5b84ffbc6c976c5fe58521
bannzai <kingkong999yhirose@gmail.com>
2022-06-16 20:42:22 +0900

    Use user primary color

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_100_days/provider/twitter_api_client.dart';
 import 'package:in_100_days/root.dart';
-import 'package:in_100_days/style/color.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,22 +16,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: AppColor.primary,
-          inputDecorationTheme: const InputDecorationTheme(
-            border: UnderlineInputBorder(
-              borderSide: BorderSide(width: 1, color: AppColor.primary),
-            ),
-          ),
-          textSelectionTheme: const TextSelectionThemeData(
-            cursorColor: AppColor.primary,
-          ),
-        ),
-        home: const Root(),
-      ),
+    return const ProviderScope(
+      child: Root(),
     );
   }
 }
