cfa47ba76ddc848efe8afb377d6de6f6199fcfe9
bannzai <kingkong999yhirose@gmail.com>
2022-06-28 20:02:42 +0900

    Fix hashtag

--- lib/components/twitter_hash_tag.dart
+++ lib/components/twitter_hash_tag.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:in_100_days/entity/goal.codegen.dart';
+import 'package:in_100_days/style/button.dart';
 import 'package:in_100_days/style/color.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TwitterHashTag extends StatelessWidget {
   final Goal goal;
@@ -8,13 +10,19 @@ class TwitterHashTag extends StatelessWidget {
   const TwitterHashTag({Key? key, required this.goal}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Text(
-      goal.fullHashTag,
-      textAlign: TextAlign.left,
-      style: const TextStyle(
-          fontWeight: FontWeight.w600,
-          fontSize: 12,
-          color: AppColor.twitterHashTag),
+    return AppTextButton(
+      text: Text(
+        goal.fullHashTag,
+        textAlign: TextAlign.left,
+        style: const TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 12,
+            color: AppColor.twitterHashTag),
+      ),
+      onPressed: () async {
+        launchUrl(Uri.parse(
+            "https://twitter.com/search?q=#${goal.fullHashTag}&src=typed_query"));
+      },
     );
   }
 }
