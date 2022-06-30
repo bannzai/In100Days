d22980ec476b2af0a70591c1c137cd3f746afb97
bannzai <kingkong999yhirose@gmail.com>
2022-06-30 11:17:56 +0900

    set with firebase options

--- lib/entrypoint.dart
+++ lib/entrypoint.dart
@@ -4,13 +4,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:in_100_days/provider/twitter_api_client.dart';
+import 'firebase_options.dart';
 
 import 'app.dart';
 
 void entrypoint() async {
   runZonedGuarded<Future<void>>(() async {
     WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
     await setupTwitterAPIClient();
     runApp(const MyApp());
