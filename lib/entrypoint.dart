15e60cad4bace96de2369668a05b5bb1d8bf43a6
bannzai <kingkong999yhirose@gmail.com>
2022-06-30 10:58:04 +0900

    Register crashlytics to entrypoint

--- lib/entrypoint.dart
+++ lib/entrypoint.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:in_100_days/provider/twitter_api_client.dart';
 
@@ -7,6 +8,7 @@ import 'app.dart';
 void entrypoint() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   await setupTwitterAPIClient();
   runApp(const MyApp());
 }
