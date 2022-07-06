ce30afae4bb4ca6c0b180edede242982f5445c2f
bannzai <kingkong999yhirose@gmail.com>
2022-07-07 07:25:28 +0900

    add price row

--- lib/features/purchase/purchase_sheet.dart
+++ lib/features/purchase/purchase_sheet.dart
@@ -5,9 +5,48 @@ class PurchaseSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Text("本気出す"),
-    ]);
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              child: Container(
+                padding: const EdgeInsets.only(
+                    bottom: 20, top: 24, left: 16, right: 16),
+                child: RichText(
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "¥120",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextSpan(
+                        text: " の本気を出す",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              onTap: () {
+                // TODO:
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
