68718f4cf0b5992803d5d56b2c961f5d7ed3f2d0
bannzai <kingkong999yhirose@gmail.com>
2022-07-07 08:30:59 +0900

    Fix purchase sheet

--- lib/features/purchase/purchase_sheet.dart
+++ lib/features/purchase/purchase_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:in_100_days/style/color.dart';
 
 class PurchaseSheet extends StatelessWidget {
   const PurchaseSheet({Key? key}) : super(key: key);
@@ -6,16 +7,28 @@ class PurchaseSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.only(bottom: 20, top: 20),
       color: Colors.white,
       child: SafeArea(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const SizedBox(
+              height: 36,
+              child: Center(
+                child: Text(
+                  "どのくらい本気を出す？",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: AppColor.textNote),
+                ),
+              ),
+            ),
+            const Divider(),
             GestureDetector(
-              child: Container(
-                padding: const EdgeInsets.only(
-                    bottom: 20, top: 24, left: 16, right: 16),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
                 child: RichText(
                   text: const TextSpan(
                     children: [
@@ -43,6 +56,135 @@ class PurchaseSheet extends StatelessWidget {
                 // TODO:
               },
             ),
+            const Divider(),
+            GestureDetector(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                child: RichText(
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "¥370",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextSpan(
+                        text: " の本気を出す",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              onTap: () {
+                // TODO:
+              },
+            ),
+            const Divider(),
+            GestureDetector(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                child: RichText(
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "¥730",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextSpan(
+                        text: " の本気を出す",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              onTap: () {
+                // TODO:
+              },
+            ),
+            const Divider(),
+            GestureDetector(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                child: RichText(
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "¥1220",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextSpan(
+                        text: " の本気を出す",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              onTap: () {
+                // TODO:
+              },
+            ),
+            const Divider(),
+            GestureDetector(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                child: RichText(
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "¥12000",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextSpan(
+                        text: " の本気を出す",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              onTap: () {
+                // TODO:
+              },
+            ),
+            const Divider(),
           ],
         ),
       ),
@@ -54,6 +196,7 @@ void showPurchaseSheet(BuildContext context) {
   showModalBottomSheet(
     backgroundColor: Colors.transparent,
     enableDrag: true,
+    isScrollControlled: true,
     context: context,
     builder: (BuildContext context) => const PurchaseSheet(),
   );
