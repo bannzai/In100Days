5d9470f402ccb70c1cb4da0ce9952d54a397aaa4
bannzai <kingkong999yhirose@gmail.com>
2022-07-14 07:06:35 +0900

    add reload button

--- lib/features/error/error_page.dart
+++ lib/features/error/error_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:in_100_days/style/button.dart';
 
 class ErrorPage extends StatelessWidget {
   final Object error;
+  final VoidCallback reload;
 
-  const ErrorPage({Key? key, required this.error}) : super(key: key);
+  const ErrorPage({Key? key, required this.error, required this.reload})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +18,12 @@ class ErrorPage extends StatelessWidget {
             const Spacer(),
             Image.asset("images/computer_sagi_error.png"),
             Text(error.toString()),
+            const SizedBox(height: 10),
+            PrimaryButton(
+                onPressed: () async {
+                  reload();
+                },
+                text: "再読み込み"),
             const Spacer(),
           ],
         ),
