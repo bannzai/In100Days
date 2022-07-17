d0bc2751fbda5ffe070022f679693abc1c6a0449
bannzai <kingkong999yhirose@gmail.com>
2022-07-17 10:44:11 +0900

    Set status bar content color

--- lib/features/goal/goal_input_sheet.dart
+++ lib/features/goal/goal_input_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_100_days/components/user_info.dart';
@@ -25,142 +26,145 @@ class GoalInputSheet extends HookConsumerWidget {
     final hashTag = "100日後に${text.value}${user.twitterID}";
     final fullHashTag = "#" + hashTag;
 
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(
-              vertical: 20, horizontal: paddingHorizontal),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Spacer(),
-              RichText(
-                textAlign: TextAlign.center,
-                text: const TextSpan(
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+                vertical: 20, horizontal: paddingHorizontal),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Spacer(),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "100日後",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: AppColor.textMain,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "に達成したい目標・なりたい自分を決めましょう",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                          color: AppColor.textMain,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                UserInfo(user: user, fullHashTag: fullHashTag),
+                const SizedBox(height: 40.5),
+                Column(
                   children: [
-                    TextSpan(
-                      text: "100日後",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: AppColor.textMain,
+                    Row(
+                      children: const [
+                        Text(
+                          "#100日後に",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 24,
+                            color: AppColor.textMain,
+                          ),
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+                    ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width -
+                            paddingHorizontal * 2,
+                      ),
+                      child: TextField(
+                        maxLength: 20,
+                        minLines: 1,
+                        maxLines: 2,
+                        textInputAction: TextInputAction.done,
+                        controller: textFieldController,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 24,
+                          color: AppColor.textMain,
+                        ),
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.only(bottom: 8),
+                          hintText: pickRandomElement<String>([
+                            "海賊王になる",
+                            "Twitterを辞める",
+                            "100Kgのバーベルをあげる",
+                            "猫を飼う",
+                            "家を買う",
+                            "お腹いっぱいにご飯を食べる"
+                          ]),
+                          hintStyle: const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 24,
+                            color: AppColor.textNote,
+                          ),
+                        ),
+                        onChanged: (_text) {
+                          text.value = _text;
+                        },
+                        onEditingComplete: () async {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                        },
                       ),
                     ),
-                    TextSpan(
-                      text: "に達成したい目標・なりたい自分を決めましょう",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                        color: AppColor.textMain,
-                      ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: [
+                        const Spacer(),
+                        Text(
+                          user.twitterID,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 24,
+                            color: AppColor.textMain,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(height: 20),
-              UserInfo(user: user, fullHashTag: fullHashTag),
-              const SizedBox(height: 40.5),
-              Column(
-                children: [
-                  Row(
-                    children: const [
-                      Text(
-                        "#100日後に",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 24,
-                          color: AppColor.textMain,
-                        ),
-                      ),
-                      Spacer(),
-                    ],
-                  ),
-                  const SizedBox(height: 12),
-                  ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxWidth: MediaQuery.of(context).size.width -
-                          paddingHorizontal * 2,
-                    ),
-                    child: TextField(
-                      maxLength: 20,
-                      minLines: 1,
-                      maxLines: 2,
-                      textInputAction: TextInputAction.done,
-                      controller: textFieldController,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 24,
-                        color: AppColor.textMain,
-                      ),
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        contentPadding: const EdgeInsets.only(bottom: 8),
-                        hintText: pickRandomElement<String>([
-                          "海賊王になる",
-                          "Twitterを辞める",
-                          "100Kgのバーベルをあげる",
-                          "猫を飼う",
-                          "家を買う",
-                          "お腹いっぱいにご飯を食べる"
-                        ]),
-                        hintStyle: const TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 24,
-                          color: AppColor.textNote,
-                        ),
-                      ),
-                      onChanged: (_text) {
-                        text.value = _text;
-                      },
-                      onEditingComplete: () async {
-                        FocusManager.instance.primaryFocus?.unfocus();
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Row(
-                    children: [
-                      const Spacer(),
-                      Text(
-                        user.twitterID,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 24,
-                          color: AppColor.textMain,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const Spacer(),
-              PrimaryButton(
-                onPressed: text.value.isEmpty
-                    ? null
-                    : () async {
-                        if (text.value.isEmpty) {
-                          return;
-                        }
-                        try {
-                          final goal = Goal(
-                            goalAction: text.value,
-                            hashTag: hashTag,
-                            createdDateTime: DateTime.now(),
-                            purchasedProducts: [],
-                          );
-                          await goalCollectionReference(userID: user.id!)
-                              .doc()
-                              .set(goal, SetOptions(merge: true));
-                          Navigator.of(context).pop();
-                        } catch (error) {
-                          showErrorAlert(context, error: error);
-                        }
-                      },
-                text: "目標を設定する",
-              )
-            ],
+                const Spacer(),
+                PrimaryButton(
+                  onPressed: text.value.isEmpty
+                      ? null
+                      : () async {
+                          if (text.value.isEmpty) {
+                            return;
+                          }
+                          try {
+                            final goal = Goal(
+                              goalAction: text.value,
+                              hashTag: hashTag,
+                              createdDateTime: DateTime.now(),
+                              purchasedProducts: [],
+                            );
+                            await goalCollectionReference(userID: user.id!)
+                                .doc()
+                                .set(goal, SetOptions(merge: true));
+                            Navigator.of(context).pop();
+                          } catch (error) {
+                            showErrorAlert(context, error: error);
+                          }
+                        },
+                  text: "目標を設定する",
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/features/sign_in/sign_in_page.dart
+++ lib/features/sign_in/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_100_days/features/error/error_alert.dart';
 import 'package:in_100_days/provider/auth.dart';
@@ -15,80 +16,83 @@ class SignInPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final stateNotifier = ref.watch(loginStateNotifierProvider.notifier);
 
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 27),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Spacer(),
-              RichText(
-                textAlign: TextAlign.center,
-                text: const TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "100日後",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: AppColor.textMain,
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 27),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Spacer(),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "100日後",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: AppColor.textMain,
+                        ),
                       ),
-                    ),
-                    TextSpan(
-                      text: "に達成したい目標・なりたい自分を決めましょう",
+                      TextSpan(
+                        text: "に達成したい目標・なりたい自分を決めましょう",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                          color: AppColor.textMain,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Image.asset("images/kids_chuunibyou_boy.png"),
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: 118,
+                  child: Column(children: const [
+                    Text(
+                      "Twitterのフォロワーにも経過報告をして応援をしてもらいましょう",
                       style: TextStyle(
-                        fontSize: 20,
+                        fontSize: 17,
                         fontWeight: FontWeight.normal,
                         color: AppColor.textMain,
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                  ],
+                    SizedBox(height: 10),
+                    Text(
+                      "※ 継続に失敗し再開する場合は課金が必要になります",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                        color: AppColor.textNote,
+                      ),
+                    ),
+                  ]),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Image.asset("images/kids_chuunibyou_boy.png"),
-              const SizedBox(height: 10),
-              SizedBox(
-                height: 118,
-                child: Column(children: const [
-                  Text(
-                    "Twitterのフォロワーにも経過報告をして応援をしてもらいましょう",
-                    style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.normal,
-                      color: AppColor.textMain,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "※ 継続に失敗し再開する場合は課金が必要になります",
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                      color: AppColor.textNote,
-                    ),
-                  ),
-                ]),
-              ),
-              const Spacer(),
-              TwitterLikeButton(
-                text: 'Twitterでログイン',
-                onPressed: () async {
-                  try {
-                    final user = await stateNotifier.asyncAction.signIn();
-                    final setUser = ref.read(setUserProvider);
-                    await setUser(user);
+                const Spacer(),
+                TwitterLikeButton(
+                  text: 'Twitterでログイン',
+                  onPressed: () async {
+                    try {
+                      final user = await stateNotifier.asyncAction.signIn();
+                      final setUser = ref.read(setUserProvider);
+                      await setUser(user);
 
-                    ref.refresh(firebaseCurrentUserProvider);
-                  } catch (error) {
-                    showErrorAlert(context, error: error);
-                  }
-                },
-              ),
-            ],
+                      ref.refresh(firebaseCurrentUserProvider);
+                    } catch (error) {
+                      showErrorAlert(context, error: error);
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
