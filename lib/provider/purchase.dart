9d9da50cab3e1f9ef2f4d828d20c0486939beb01
bannzai <kingkong999yhirose@gmail.com>
2022-07-20 06:35:50 +0900

    rename purchase items

--- lib/provider/purchase.dart
+++ lib/provider/purchase.dart
@@ -1,16 +1,28 @@
+import 'dart:io';
+
 import 'package:in_100_days/flavors.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:riverpod/riverpod.dart';
 
 List<String> productIdentifiers() {
   if (F.appFlavor == Flavor.DEV) {
-    return [
-      "in100days_dev_120_consume",
-      "in100days_dev_370_consume",
-      "in100days_dev_730_consume",
-      "in100days_dev_1220_consume",
-      "in100days_dev_12000_consume",
-    ];
+    if (Platform.isIOS) {
+      return [
+        "in100days_dev_120_consume",
+        "in100days_dev_370_consume",
+        "in100days_dev_730_consume",
+        "in100days_dev_1220_consume",
+        "in100days_dev_12000_consume",
+      ];
+    } else {
+      return [
+        "in100days_dev2_120_consume",
+        "in100days_dev2_370_consume",
+        "in100days_dev2_730_consume",
+        "in100days_dev2_1220_consume",
+        "in100days_dev2_12000_consume",
+      ];
+    }
   } else {
     return [
       "in100days_120_consume",
